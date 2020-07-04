a37d0f4ce2601188f39484adc437631ea4579d63
SCZXH <49623973+SCZXH@users.noreply.github.com>
2020-07-04 00:38:39 -0400

    Add files via upload

---
+++ CRAG.m
@@ -0,0 +1,94 @@
+function CRAG(data_name,peak_type,varargin)
+%%%%%%Call hotspots based on fragment information saved in txt file in folder 'data_name' (i.e. BH01).
+%%%%%%Call hotspots based on coverage and fragment length. (Peak_type==1: IFS. Peak_type==2: GC bias corrected IFS.)
+%%%optional parameters:
+%%global_p: global p-value cut off
+%%local_p: p-value cut-off for local test
+%%fdr: cut-off
+%%distance: Distance cut-off to merge the significant regions nearby.
+%%enrichment:whether or not do enrichment for the hotspots:  argument: 'enrichment', 0 or 1. default: 1.
+if ischar(peak_type)
+    peak_type=str2double(peak_type);
+end
+if nargin<2;error('There should be at least two input parameters');end
+if nargin==3;error('Input parameters error£¡');end
+
+global_p=0.00001;
+local_p=0.00001;
+fdr=0.01;
+distance=200;
+enrichment=1;
+para_val=varargin;
+while length(para_val)>=2
+    prop =para_val{1};
+    val=para_val{2};
+    if ischar(val)
+        val=str2double(val);
+    end
+    para_val=para_val(3:end);
+    switch prop
+        case 'global_p'
+            global_p=val;
+        case 'local_p'
+            local_p=val;
+        case 'fdr'
+            fdr=val;
+        case 'distance'
+            distance=val;
+        case 'enrichment'
+            enrichment=val;
+        otherwise
+            error('Input parameters error')
+    end
+end
+
+
+%%Use function 'txt_read' to read fragment informaion from txt file
+%%and map the fragment information. All the data was saved in 'data_n/' as 'chri.mat'
+for i=1:22
+    txt_read(data_name,i);
+end
+
+
+%%The output was saved in 'outpeak_n/' as 'chri_peak.mat'.
+for i=1:22
+    Hotspot_call(data_name,peak_type,i,global_p,local_p,fdr);
+end
+
+%%%Merge all the hotspots and the output was saved in 'result_n/' as 'peak_all.mat'
+Hotspot_merge(data_name,distance);
+
+%%Write the hotspots in 'result_n/' as 'hotspots.bed'
+Hotspot_out(data_name);
+
+%%Plot the fragment pattern around hotspots. The output was saved in
+%%'result_n/' as 'peak_plot.fig/pdf'
+Hotspot_IFS_plot(data_name);
+
+%%%%%Do enrichment for the hotspots
+if (enrichment==1)
+    %%Plot the hotspot pattern around TSS. The output was saved in
+    %%'result_n/' as 'TSS_plot.fig/pdf'
+    Hotspot_TSS(data_name);
+    
+    %%Plot the hotspot pattern around CTCF. The output was saved in
+    %%'result_n/' as 'CTCF_plot.fig/pdf'
+    Hotspot_CTCF(data_name);
+    
+    %%%%%%%%%Calculate the overlap number of hotspots for each chromHMM, which
+    %%%%%%%would laters used to chromHMM enrichment
+    %%%%%The output was saved in 'result_n/' as 'enri.mat'
+    Hotspot_enrichment(data_name);
+    
+    %%%%%%%%%Calculate the overlap number of random hotspots for each chromHMM, which
+    %%%%%%%would laters used to chromHMM enrichment
+    %%%%%The output was saved in 'result_n/' as 'enri_randm.mat'
+    Hotspot_rand_enrichment(data_name);
+    
+    %%%%%%%Use fisher exact test to test do the chromHMM enrichment, based on
+    %%%%%%%the overlap value of the hotspots with each chromHMM, and the overlap value of the random hotspots with each chromHMM
+    %%%%%%%The output was saved in 'result_n/' as 'Enrichment_plot.fig'
+    Hotspot_enrichment_fisher(data_name);
+end
+
+end
---
+++ Hotspot_enrichment.m
@@ -0,0 +1,65 @@
+function Hotspot_enrichment(data_name)
+%%%%%%%Calculate the overlap number of hotspots for each chromHMM, which
+%%%%%%%would laters used to chromHMM enrichment
+%%%%%%%%%data_name: The file name of the data set. i.e. BH01
+
+file_path = strcat('./',data_name);
+out_res= strcat(file_path,'/result_n/');
+file_path = strcat(file_path,'/result_n/peak_all.mat');
+
+load (file_path);   %%load all the hotspots data
+
+path(path,'./Basic_info/');
+
+load ./Basic_info/chromosome_info.mat;
+
+path(path,'./Basic_info/chrm_state/');   %%Path for the chromHMM data
+
+listing = dir('./Basic_info/chrm_state/');
+
+cou=0;
+for i=1:length(listing)   %%obtain all the names of chromHMM data
+    lo=strfind(listing(i).name,'E');
+    if ~isempty(lo)
+        cou=cou+1;
+        name{cou,1}=listing(i).name;
+    end
+end
+
+
+n=length(name);
+
+
+ma=zeros(15,n);
+chrm_state=zeros(15,n);
+
+for i=1:n
+    te=strcat('./Basic_info/chrm_state/',name{i,1});
+    
+    for j=1:22
+        temp_data=peak_a(peak_a(:,1)==j,2:3); %%Hotspots in the current chromosome
+        if ~isempty(temp_data)
+            te2=strcat(te,'/chr');
+            te2=strcat(te2,num2str(j));
+            te2=strcat(te2,'_s.mat');
+            load (te2); %%Load the chroHMM data for the current chromosome
+            num=length(temp_data(:,1));
+            for k=1:num
+                t=loc_e(temp_data(k,1):temp_data(k,2),1);  %%The Chromatin states in the region of the current hotspot
+                seed=unique(t);
+                s_num=length(seed);
+                if s_num==1
+                    ma(seed,i)=ma(seed,i)+1;  %%If the Chromatin state has overlap with the current hotspots, then add 1.
+                else
+                    ma(seed(:),i)=ma(seed(:),i)+1;
+                end
+            end
+            chrm_state(:,i)=chrm_state(:,i)+state_num;
+        end
+    end
+end
+
+save_name=strcat(out_res,'enri.mat');
+save((save_name),'chrm_state','ma','name');  %%Save the overlap information
+
+end
