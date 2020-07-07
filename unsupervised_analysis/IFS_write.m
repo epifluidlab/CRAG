1f5902771295481cf49245780228aecca7483bb7
SCZXH <49623973+SCZXH@users.noreply.github.com>
2020-07-07 01:54:14 -0400

    Update IFS_write.m

--- unsupervised_analysis/IFS_write.m
+++ unsupervised_analysis/IFS_write.m
@@ -2,7 +2,14 @@ function IFS_write(data_name)
 %%%%%%%%%%%%%%Use IFS to write the fragment information (txt file) into .mat file
 %%%%%%%%%%%%%%data_name:The sample name
 
-%%%%%%%%%Use txt_read.m to write all the fragment information chromose.
+%%%%%%Add the path of the funtions used in this pipeline as workplace
+current_path=pwd;
+lo=strfind(current_path,'/');
+parent_path=current_path(1,1:(lo(end)-1));
+addpath(genpath(parent_path));
+
+
+%%%%%%%%%Use txt_read.m to write IFS for all the chromoses.
 for i=1:22
     txt_read(data_name,i);
 end
