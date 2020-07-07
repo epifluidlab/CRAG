12a22ac3923f8f6018fe8bb7aa149db51ac3477d
SCZXH <49623973+SCZXH@users.noreply.github.com>
2020-07-07 18:08:15 -0400

    Update IFS_write.m

--- manuscript/unsupervised_analysis/IFS_write.m
+++ manuscript/unsupervised_analysis/IFS_write.m
@@ -2,6 +2,12 @@ function IFS_write(data_name)
 %%%%%%%%%%%%%%Use IFS to write the fragment information (txt file) into .mat file
 %%%%%%%%%%%%%%data_name:The sample name
 
+%%%%%%Add the path of the funtions used in this pipeline as workplace
+current_path=pwd;
+lo=strfind(current_path,'/');
+parent_path=current_path(1,1:(lo(end)-1));
+addpath(genpath(parent_path));
+
 %%%%%%%%%Use txt_read.m to write all the fragment information chromose.
 for i=1:22
     txt_read(data_name,i);
